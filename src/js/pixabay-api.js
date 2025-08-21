a0a732d39239d6d067153ef3a5765c7dec9479ac
Yevhenii Shashurkin <lifeinkiev@i.ua>
2025-08-22 00:57:22 +0300

    hmw11-1

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,20 +1,19 @@
 import axios from 'axios';
 
 export function getImagesByQuery(query) {
-  axios
+  return axios
     .get('https://pixabay.com/api/', {
       params: {
         key: '51870132-66db1267a2a3823963ca5e362',
-        q: `${query}`,
+        q: query,
         image_type: 'photo',
         orientation: 'horizontal',
         safesearch: true,
       },
     })
-    .then(res => {
-      console.log(res.data);
-    })
+    .then(res => res.data)
     .catch(err => {
-      console.log(err);
+      console.log('ERROR');
+      throw err;
     });
 }
--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
-import { getImagesByQuery } from './js/pixabay-api';
-
-// import {
-//   createGallery,
-//   clearGallery,
-//   showLoader,
-//   hideLoader,
-// } from './js/render-functions';
-
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+
+import { getImagesByQuery } from './js/pixabay-api';
+
+import {
+  createGallery,
+  clearGallery,
+  showLoader,
+  hideLoader,
+} from './js/render-functions';
 
 const formEl = document.querySelector('.form');
 
@@ -17,7 +17,30 @@ formEl.addEventListener('submit', e => {
   const query = e.currentTarget.elements['search-text'].value
     .trim()
     .toLowerCase();
-  console.log(query);
 
-  getImagesByQuery(query);
+  clearGallery();
+  showLoader();
+
+  getImagesByQuery(query)
+    .then(data => {
+      hideLoader();
+      if (!data.hits || data.hits.length === 0) {
+        iziToast.error({
+          title: 'Error!!!',
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+        });
+        return;
+      }
+
+      createGallery(data.hits);
+    })
+    .catch(err => {
+      hideLoader();
+      iziToast.error({
+        title: 'Error',
+        message: 'Something went wrong. Please try again later.',
+      });
+    });
+  formEl.reset();
 });
